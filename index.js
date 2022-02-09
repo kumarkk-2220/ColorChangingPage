b287f65ec4e04c5c208df8d31135fb2c4b5d0a94
Kp Kumar <kumarkk.2220@gmail.com>
2022-02-09 11:00:02 +0530

    Color Changing Web Page On Click

---
+++ index.js
@@ -0,0 +1,39 @@
+let listOfColors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "pink",
+    "black",
+    "grey",
+    "purple",
+    "violet",
+    "brown",
+    "indigo",
+];
+
+randomColor = () => {
+    let randomNum = Math.floor(Math.random() * listOfColors.length);
+    return listOfColors[randomNum];
+};
+
+let button = document.querySelector(".btn");
+button.addEventListener("click", function (color) {
+    color = randomColor();
+    document.querySelector(".sub-heading").innerHTML = `Applied Color: ${
+        color[0].toUpperCase() + color.substring(1).toLowerCase()
+    }`;
+    document.querySelector("body").style.backgroundColor = color;
+    if (color === "black") {
+        document.querySelector("body").style.color = "white";
+    } else {
+        document.querySelector("body").style.color = "black";
+    }
+});
+
+let reset = document.querySelector(".reset");
+reset.addEventListener("click", function () {
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
+    document.querySelector(".sub-heading").innerHTML = "";
+});
